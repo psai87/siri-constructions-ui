10804bb3adc37bca97877f108d4bb3c51074a39b
psai87 <PSAI87@GMAIL.COM>
2025-08-28 01:06:30 -0700

    fd

--- src/modules/contact/ContactPage.tsx
+++ src/modules/contact/ContactPage.tsx
@@ -1,7 +1,7 @@
 
 export default function ContactPage() {
     return (
-        <div className="min-h-screen bg-gray-100 text-gray-900 p-12 md:p-12 py-20">
+        <div className="min-h-screen bg-gray-100 text-gray-900 px-10 py-20">
             <section className="max-w-7xl mx-auto py-8 md:py-16">
                 <h1 className="text-4xl sm:text-5xl font-extrabold mb-8 md:mb-12 text-center text-blue-600">Contact Us</h1>
 
--- src/modules/service/ViewServicePage.tsx
+++ src/modules/service/ViewServicePage.tsx
@@ -33,7 +33,7 @@ export default function ViewServicePage({setAlerts}: AlertsProps) {
     };
 
     return (
-        <div className="p-12 md:p-12 py-20">
+        <div className="px-10 py-20">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl">
                 {services.map((service) => (
                     <div
