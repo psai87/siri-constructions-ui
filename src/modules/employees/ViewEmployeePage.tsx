99d08a340e7d1fe145bca8247867a210da75c150
psai87 <PSAI87@GMAIL.COM>
2025-08-28 21:11:00 -0700

    fx

--- src/modules/employees/ViewEmployeePage.tsx
+++ src/modules/employees/ViewEmployeePage.tsx
@@ -35,7 +35,7 @@ export default function ViewEmployeePage({setAlerts}: AlertsProps) {
 
     return (
         <div className="px-10 py-25">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl">
+            <div className="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6">
                 {employees.map((employee) => (
                     <div
                         key={employee.id}
