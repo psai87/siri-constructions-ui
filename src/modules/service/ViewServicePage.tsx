3b36c10faea8f0468a6cb82602e1d3e4420e1c78
psai87 <PSAI87@GMAIL.COM>
2025-08-28 21:02:20 -0700

    fx

--- src/modules/service/ViewServicePage.tsx
+++ src/modules/service/ViewServicePage.tsx
@@ -34,7 +34,7 @@ export default function ViewServicePage({setAlerts}: AlertsProps) {
 
     return (
         <div className="px-10 py-25">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 {services.map((service) => (
                     <div
                         key={service.id}
